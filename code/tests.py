ae28c22182b466e537a5c797a071605cb581180a
amet-vikram13 <amet97vikram@gmail.com>
2024-11-06 23:25:19 -0500

    implemented test for clarkson coreset

--- code/tests.py
+++ code/tests.py
@@ -1,7 +1,8 @@
-from archetypalanalysis import farthestPointsSetUsingMinMax
-from archetypalanalysis import isConvexCombination
-from archetypalanalysis import findWitnessVector
 import numpy as np
+
+from experiments import *
+from utils import *
+
 
 def getPreData_X():
     return np.array([[1,2,3],[3,4,5],[5,6,7]])
@@ -55,11 +56,65 @@ def test_findWitnessVector():
     print("Witness Vector: ", witness_vector)
     print("----- findWitnessVector() passed -----\n")
 
+def test_ijcnn1_convex_combination():
+    X, y = load_data("ijcnn1")
+
+    print("Applying farthestPointsSetUsingMinMax algorithm")
+    ind_E = farthestPointsSetUsingMinMax(X)
+    print("Length of ind_E: ", len(ind_E))
+    print(ind_E)
+    ind_S = np.setdiff1d(np.arange(len(X)), np.array(ind_E)).tolist()
+
+    # print(isConvexCombination(X, ind_E, ind_S[0]))
+
+    cc_count = 0
+    for s in ind_S:
+        if isConvexCombination(X, ind_E, s):
+            cc_count += 1
+
+    ncc_count = len(ind_S) - cc_count
+
+    print("Number of points in ind_S that are convex combinations of ind_E: ", cc_count)
+    print("Number of points in ind_S that are not convex combinations of ind_E: ", ncc_count)
+
+def test_ijcnn1_witness_vector():
+    X, y = load_data("ijcnn1")
+
+    print("Applying farthestPointsSetUsingMinMax algorithm")
+    ind_E = farthestPointsSetUsingMinMax(X)
+    print("Length of ind_E: ", len(ind_E))
+    print(ind_E)
+    ind_S = np.setdiff1d(np.arange(len(X)), np.array(ind_E)).tolist()
+
+    # print(findWitnessVector(X, ind_E, ind_S[0]))
+
+    # takes too long to run
+    for s in ind_S:
+        print(findWitnessVector(X, ind_E, ind_S[s]))
+
+def test_ijcnn1_clarkson_coreset():
+    X, y = load_data("ijcnn1")
+
+    print("Applying farthestPointsSetUsingMinMax algorithm")
+    ind_E = farthestPointsSetUsingMinMax(X)
+    print("Length of ind_E: ", len(ind_E))
+    print(ind_E)
+    ind_S = np.setdiff1d(np.arange(len(X)), np.array(ind_E)).tolist()
+
+    print("Applying clarkson coreset algorithm")
+    # takes too long to run
+    X_C = clarkson_coreset(X, ind_E, ind_S)
+
+    print("Length of X_C: ", len(X_C))
+
 def run_tests():
     test_farthestPointsSetUsingMinMax()
     test_isConvexCombination()
     test_isNotConvexCombination()
     test_findWitnessVector()
+    # test_ijcnn1_convex_combination() # takes too long to run
+    # test_ijcnn1_witness_vector() # takes too long to run
+    # test_ijcnn1_clarkson_coreset() # takes too long to run
 
 if __name__ == "__main__":
     run_tests()
